b26a071f7512ec2dd18e08b79f485c9a8ae3f5a6
Ikraam <ibndaanis@gmail.com>
2024-03-31 04:15:30 +0100

    removed comments

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -61,30 +61,6 @@ export const About = () => {
           </div>
         </div>
       </article>
-      {/* <Content>
-        Learning to code has been a tough journey. With the help of{" "}
-        <a
-          href="https://www.youtube.com/channel/UCSKr_gILoWAETdYpAoCxB0w/channels?view=56&shelf_id=0"
-          target="blank"
-          rel="noreferrer"
-          className="hover:text-textWhite text-lightPink rounded-lg font-medium leading-[1.2] transition-all hover:bg-[#ff0000] hover:px-2 hover:py-[2px]"
-        >
-          developers on YouTube,
-        </a>{" "}
-        I have learnt web development from scratch. I remember once being
-        overwhelmed by HTML and SCSS, now I&#39;m creating this website in React
-        and TypeScript.
-      </Content>
-      <Content>
-        Through YouTube, Udemy, Scrimba, and other resources, I have learnt
-        several technologies. HTML, CSS, SCSS, Styled Components, JavaScript,
-        ES6+, React, Redux, NodeJs, Express and MongoDB, just to name a few.
-      </Content>
-      <Content>
-        I&#39;m a quick learner and eager to find out new things. I&#39;m never
-        satisfied with mediocre. If I do something, I do it with perfection.
-        Look below.
-      </Content> */}
     </section>
   );
 };
