9eafbb6a803f4f809db218ae55347675905cabd5
Ikraam <ibndaanis@gmail.com>
2024-12-27 17:22:43 +0000

    cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,8 @@
-import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig, passthroughImageService } from "astro/config";
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
   integrations: [tailwind(), react()],
   image: {
     service: passthroughImageService()
