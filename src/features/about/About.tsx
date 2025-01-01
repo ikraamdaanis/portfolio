ed0f531df84f1cdae2c4d9b99c1ef88621bd8aee
Ikraam <ibndaanis@gmail.com>
2025-01-01 11:23:48 +0000

    removed old about page

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,62 +1,48 @@
-import { Image } from "components/Image";
-import { Pill } from "components/Pill";
-import { Sophos } from "components/icons/Sophos";
-import { TypeScript } from "components/icons/TypeScript";
+import { Title } from "components/Title";
+import { cn } from "utils/cn";
+import { defaultTextStyles } from "utils/consts";
 
-/** Section where I talk about my story. */
 export const About = () => {
+  const paragraphStyles = cn("mb-12", defaultTextStyles);
+
   return (
-    <section className="relative mx-auto mb-[8rem] mt-40 flex w-full max-w-screen-md flex-col justify-center px-4">
-      <article>
-        <h1 className="mb-8 text-balance text-2xl font-medium sm:text-3xl">
-          I'm Ikraam - a{" "}
-          <span className="text-brand-400">software engineer</span>
-        </h1>
-        <h3 className="font-semibold uppercase tracking-widest text-brand-400">
-          About Me
-        </h3>
-        <p className="text-balance leading-8 text-textWhite">
-          I'm a Software Engineer from London, UK. I'm currently working as a{" "}
-          <Pill className="inline-flex items-center gap-1">
-            <TypeScript className="size-4" />
-            TypeScript
-          </Pill>{" "}
-          dev for{" "}
-          <Pill className="inline-flex items-center gap-1">
-            <Sophos className="text-[#005bc8]" />
-            Sophos
-          </Pill>
-          . Coding is my passion, and I absolutely love what I do. Every day, I
-          get to tackle new challenges and create impactful products that make a
-          difference. It's been an incredible journey so far and I'm excited to
-          see where my career takes me.
-        </p>
-      </article>
-      <article className="my-8 flex flex-col gap-2">
-        <h3 className="font-semibold uppercase tracking-widest text-brand-400">
-          Some side projects over the years
-        </h3>
-        <div className="columns-1 gap-4 sm:columns-1">
-          <div className="relative mb-4 aspect-[16/10] overflow-hidden">
-            <Image
-              src="/images/discourse-1.webp"
-              alt="work"
-              className="rounded-sm object-cover object-top sm:object-center"
-            />
+    <section
+      id="about"
+      className="container relative mx-auto mb-32 px-4 max-sm:mb-0 sm:max-w-screen-md"
+    >
+      <Title title="About Me" />
+      <article className="prose prose-invert max-w-none">
+        <div className="space-y-16 max-sm:space-y-8">
+          <div>
+            <h2 className="sr-only">My Background</h2>
+            <p className={paragraphStyles}>
+              I'm a Software Engineer from London, UK. I'm currently working as
+              developer for a small startup. Coding is my passion, and I
+              absolutely love what I do. Every day, I get to tackle new
+              challenges and create impactful products that make a difference.
+              It's been an incredible journey so far and I'm excited to see
+              where my career takes me.
+            </p>
           </div>
-          <div className="relative mb-4 aspect-[16/10] overflow-hidden">
-            <Image
-              src="/images/form-builder.webp"
-              alt="work"
-              className="rounded-sm object-cover object-top sm:object-center"
-            />
+          <div>
+            <h2 className="sr-only">Technical Experience</h2>
+            <p className={paragraphStyles}>
+              After a few years in the industry, I've built several full-stack
+              applications used by hundreds of thousands of customers. I enjoy
+              building web apps that are impactful and valuable to our
+              customers. These days I focus a lot on writing clean code that's
+              easy to maintain and making sure everything performs well.
+            </p>
           </div>
-          <div className="relative mb-4 aspect-[16/10] overflow-hidden">
-            <Image
-              src="/images/todoer-1.webp"
-              alt="work"
-              className="rounded-sm object-cover object-top sm:object-center"
-            />
+          <div>
+            <h2 className="sr-only">Work Approach</h2>
+            <p className={paragraphStyles}>
+              I love working with other developers and sharing what I know. The
+              tech world keeps changing and that's what makes it exciting -
+              there's always something new to learn and better ways to solve
+              problems. When I build something, I make sure it's done right,
+              with perfection.
+            </p>
           </div>
         </div>
       </article>
