6ee6931d256924964f6df0130410aed083c21e13
Ikraam <ibndaanis@gmail.com>
2025-01-01 11:22:23 +0000

    github pages

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig, passthroughImageService } from "astro/config";
 
 export default defineConfig({
+  site: "https://ikraamdaanis.github.io",
+  base: "portfolio",
   integrations: [tailwind(), react()],
   image: {
     service: passthroughImageService()
