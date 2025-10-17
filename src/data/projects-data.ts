3e0e26150d53a2d0f197643ad1bf773854c290cd
Ikraam <ibndaanis@gmail.com>
2025-10-17 20:19:03 +0400

    various refactors, using coloured icons

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -3,24 +3,29 @@ import discourse2 from "assets/images/discourse-2.webp";
 import todoer1 from "assets/images/todoer-1.webp";
 import todoer2 from "assets/images/todoer-2.webp";
 import type { ImageMetadata } from "astro";
-import type { SimpleIcon } from "simple-icons";
-import {
-  siBun,
-  siFirebase,
-  siNextdotjs,
-  siPostgresql,
-  siPrisma,
-  siReact,
-  siReactquery,
-  siReactrouter,
-  siRedux,
-  siStyledcomponents,
-  siTailwindcss
-} from "simple-icons";
 
 type Technology = {
   name: string;
-  icon?: SimpleIcon | "zustand";
+  icon?:
+    | "nextjs"
+    | "react-query"
+    | "zustand"
+    | "firebase"
+    | "react-router"
+    | "react"
+    | "postgres"
+    | "prisma"
+    | "bun"
+    | "tailwind"
+    | "styled-components"
+    | "redux"
+    | "react-router"
+    | "react-query"
+    | "zustand"
+    | "💅";
+  bg?: string;
+  text?: string;
+  iconColor?: string;
 };
 
 export type Project = {
@@ -43,16 +48,64 @@ export const projects: Project[] = [
     description2:
       "It has several features such as real-time chat, voice and video calls, and the ability to create and join servers. Servers can have multiple channels and users can send messages in real-time. Admins can set up roles and permissions for users.",
     list1: [
-      { name: "React.js", icon: siReact },
-      { name: "Next.js", icon: siNextdotjs },
-      { name: "Zustand", icon: "zustand" },
-      { name: "React Query", icon: siReactquery }
+      {
+        name: "React.js",
+        icon: "react",
+        bg: "#0d1117",
+        text: "#FFFFFF",
+        iconColor: "#61DAFB"
+      },
+      {
+        name: "Next.js",
+        icon: "nextjs",
+        bg: "#000",
+        text: "#fff",
+        iconColor: "#333"
+      },
+      {
+        name: "Zustand",
+        icon: "zustand",
+        bg: "#1a1511",
+        text: "#FFFFFF",
+        iconColor: "#8B7355"
+      },
+      {
+        name: "React Query",
+        icon: "react-query",
+        bg: "#3f1316",
+        text: "#FFFFFF",
+        iconColor: "#FF4154"
+      }
     ],
     list2: [
-      { name: "Tailwind CSS", icon: siTailwindcss },
-      { name: "PostgresSQL", icon: siPostgresql },
-      { name: "Prisma", icon: siPrisma },
-      { name: "Bun", icon: siBun }
+      {
+        name: "Tailwind CSS",
+        icon: "tailwind",
+        bg: "#0b1e26",
+        text: "#FFFFFF",
+        iconColor: "#06B6D4"
+      },
+      {
+        name: "PostgresSQL",
+        icon: "postgres",
+        bg: "#0d1426",
+        text: "#FFFFFF",
+        iconColor: "#4169E1"
+      },
+      {
+        name: "Prisma",
+        icon: "prisma",
+        bg: "#0d1117",
+        text: "#FFFFFF",
+        iconColor: "#FFFFFF"
+      },
+      {
+        name: "Bun",
+        icon: "bun",
+        bg: "#1a1512",
+        text: "#FFFFFF",
+        iconColor: "#FBF0DF"
+      }
     ],
     repo: "https://github.com/ikraamdaanis/discourse",
     website: "https://discourseapp.vercel.app/",
@@ -66,13 +119,43 @@ export const projects: Project[] = [
     description2:
       "I have implemented create, read, update and delete features for the tasks. They can be sorted by the due date and alphabetically. All tasks can be updated and completed and entire projects can be deleted. I also cloned the Todoist home screen for the landing page.",
     list1: [
-      { name: "React.js", icon: siReact },
-      { name: "Redux", icon: siRedux },
-      { name: "Firebase", icon: siFirebase }
+      {
+        name: "React.js",
+        icon: "react",
+        bg: "#0d1117",
+        text: "#FFFFFF",
+        iconColor: "#61DAFB"
+      },
+      {
+        name: "Redux",
+        icon: "redux",
+        bg: "#1a0e26",
+        text: "#FFFFFF",
+        iconColor: "#764ABC"
+      },
+      {
+        name: "Firebase",
+        icon: "firebase",
+        bg: "#350c02",
+        text: "#FFFFFF",
+        iconColor: "#FFCA28"
+      }
     ],
     list2: [
-      { name: "React Router", icon: siReactrouter },
-      { name: "Styled Components", icon: siStyledcomponents }
+      {
+        name: "React Router",
+        icon: "react-router",
+        bg: "#260d0f",
+        text: "#FFFFFF",
+        iconColor: "#CA4245"
+      },
+      {
+        name: "Styled Components",
+        icon: "💅",
+        bg: "#291520",
+        text: "#FFFFFF",
+        iconColor: "#DB7093"
+      }
     ],
     repo: "https://github.com/ikraamdaanis/todoer",
     website: "https://todoer-app.netlify.app/",
