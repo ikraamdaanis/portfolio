3c6060836f7b4d666925c304bd3a8d01c8f5cabd
Ikraam <ibndaanis@gmail.com>
2025-10-19 19:21:33 +0400

    seo improvements

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,12 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://ikraam.dev",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   adapter: cloudflare(),
   image: {
     service: {
